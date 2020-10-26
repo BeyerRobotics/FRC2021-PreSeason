576f606ffdd9da6193aab49fb57cb2496708fb7f
benjii33 <benfich000@gmail.com>
2020-10-25 22:14:11 -0700

    Fixed xboxSplitArcade error with call to drivetrain closed loop yawbydrive

--- src/main/java/frc/robot/commands/ClosedLoopXboxControllerSplitArcade.java
+++ src/main/java/frc/robot/commands/ClosedLoopXboxControllerSplitArcade.java
@@ -35,7 +35,7 @@ public class ClosedLoopXboxControllerSplitArcade extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.ClosedLoopControllerSplitArcade(joy);
+    drivetrain.ClosedLoopControllerSplitArcadeYawDrive(joy);
   }
 
   // Called once the command ends or is interrupted.
